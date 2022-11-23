b32cdc73b5dfb332ed08f2b453155aae776b2332
albanba <albanba@gmail.com>
2022-11-22 20:43:06 -0600

    y

---
+++ lesson10/scripts/weatherapi.js
@@ -0,0 +1,38 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=270e38ed066873b2c1c99cd439fcfcac';
+
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  function capitalize(string){
+    return `${string.charAt(0).toUpperCase()}${string.slice(1)}`
+  }
+
+  apiFetch()
+
+  function  displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  
+    const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`;
+    const desc = capitalize(weatherData.weather[0].description);
+  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+  }
