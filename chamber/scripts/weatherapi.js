631e1c79393c0895a9edb09b1e6ac4194e9c5551
albanba <albanba@gmail.com>
2022-11-24 07:56:57 -0600

    y

---
+++ chamber/scripts/weatherapi.js
@@ -0,0 +1,41 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Turrialba,CostaRica&units=imperial&appid=270e38ed066873b2c1c99cd439fcfcac';
+const mph = document.querySelector('#mph');
+const condition = document.querySelector('#condition');
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  function capitalize(string){
+    return `${string.charAt(0).toUpperCase()}${string.slice(1)}`
+  }
+
+  apiFetch()
+
+  function  displayResults(weatherData) {
+    currentTemp.textContent = `Temperature: ${weatherData.main.temp.toFixed(0)} °F`;
+    mph.textContent = `Wind Speed: ${weatherData.wind.speed} MPH`;
+    const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`;
+    const desc = capitalize(weatherData.weather[0].description);
+  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    
+    condition.textContent = desc;
+    console.log(desc);
+  }
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,7 +1,12 @@
 
 const temp = document.getElementById('temp').innerHTML
-const speed = document.getElementById("speed").innerHTML
+const speed = document.getElementById("mph").innerHTML
 
-let wChill = 35.74 + (0.6215 * temp) - (35.75 * (speed **0.16)) + (0.4275 * temp * (speed ** 0.16))
+if (temp <= 50 && mph >3){ 
+    let wChill = 35.74 + (0.6215 * temp) - (35.75 * (speed **0.16)) + (0.4275 * temp * (speed ** 0.16));
+    document.getElementById("wChill").innerText =`Wind Chill: ${wChill.toFixed(2)} °F`;
+} 
 
-document.getElementById("wChill").innerText = wChill.toFixed(2);
+else {document.getElementById("wChill").innerText = "Wind Chill: N/A";  
+} 
+
